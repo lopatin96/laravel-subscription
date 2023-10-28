38f659089312fee171be39bbdf5cdcecdbd618a8
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-10-28 21:33:54 +0200

    Subscription model

---
+++ src/Models/Subscription.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Atin\LaravelSubscription\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Subscription extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
