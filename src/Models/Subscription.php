473fa9fcd30baa5a6b8584365eb4857ed62c61bb
vladyslavlopatin <ilopatin3@gmail.com>
2023-11-16 19:27:58 +0100

    added $casts

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -10,6 +10,10 @@ class Subscription extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'ends_at' => 'datetime',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
