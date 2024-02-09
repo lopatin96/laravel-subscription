0a31f9a8a34d69b44e3228ba4a7c64229b869c53
vladyslavlopatin <ilopatin3@gmail.com>
2024-02-09 21:44:29 +0100

    HasNovaBadges

--- src/Models/Subscription.php
+++ src/Models/Subscription.php
@@ -2,13 +2,14 @@
 
 namespace Atin\LaravelSubscription\Models;
 
+use Atin\LaravelNovaBadges\Traits\HasNovaBadges;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
 class Subscription extends Model
 {
-    use HasFactory;
+    use HasFactory, HasNovaBadges;
 
     protected $casts = [
         'trial_ends_at' => 'datetime',
