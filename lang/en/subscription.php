21c1374d7ad6003775b7fd0dbce43e258c85cf5f
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2024-02-29 16:36:09 +0100

    past_due

--- lang/en/subscription.php
+++ lang/en/subscription.php
@@ -3,6 +3,7 @@
 return [
 
     'incomplete_or_pending' => 'Your payment is still being processed. It can take some time.',
+    'past_due' => 'Payment failed. Try a different payment method, or they can retry the payment at a later date.',
 
     'trial_until' => '{0} Today is the last day of your trial period. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.|{1} You have :days day of trial period left. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.|[2,*] You have :days days of trial period left. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.',
     'trial_period_is_over' => 'The trial period is over. To continue using the service <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.',
