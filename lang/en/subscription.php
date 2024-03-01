b34d95735092be62881f23621ab879bccfbe8cce
vladyslavlopatin <ilopatin3@gmail.com>
2024-03-01 12:19:44 +0100

    If this process takes too long, your bank may have rejected your payment. If this is the case, try another payment method or write to us in support (bottom right corner of the screen).

--- lang/en/subscription.php
+++ lang/en/subscription.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'incomplete_or_pending' => 'Your payment is still being processed. It can take some time.',
+    'incomplete_or_pending' => 'Your payment is still being processed. It can take some time. If this process takes too long, your bank may have rejected your payment. If this is the case, try <a href="/billing" class="font-semibold underline">another payment method</a> or write to us in support (bottom right corner of the screen).',
     'past_due' => 'Payment failed. <a href="/billing" class="font-semibold underline">Try a different payment method</a>, or retry the payment at a later date.',
 
     'trial_until' => '{0} Today is the last day of your trial period. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.|{1} You have :days day of trial period left. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.|[2,*] You have :days days of trial period left. Don’t forget to <a href="/billing" class="font-semibold underline">choose a subscription plan</a>.',
