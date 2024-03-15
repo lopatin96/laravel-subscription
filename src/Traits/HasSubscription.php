b5fb702c809881820931abaccfd7c104ee0610fd
vladyslavlopatin <ilopatin3@gmail.com>
2024-03-15 23:08:52 +0100

    getSubscribedPlanIdx

--- src/Traits/HasSubscription.php
+++ src/Traits/HasSubscription.php
@@ -3,31 +3,61 @@
 namespace Atin\LaravelSubscription\Traits;
 
 use Atin\LaravelSubscription\Models\Subscription;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 trait HasSubscription
 {
-    public function stripeSubscriptions(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function stripeSubscriptions(): HasMany
     {
         return $this->hasMany(Subscription::class);
     }
 
-    public function stripeSubscription(): \Illuminate\Database\Eloquent\Relations\HasOne
+    public function stripeSubscription(): HasOne
     {
         return $this->hasOne(Subscription::class)
             ->latest('id');
     }
 
-    public function getSubscribedPlan(): int|null
+    public function getSubscribedPlan(): ?array
     {
-        if ($this->subscribed()) {
+        return $this->getSubscribedPlanData();
+    }
+
+    public function getSubscribedPlanIdx(): ?int
+    {
+        return $this->getSubscribedPlanData()['idx'];
+    }
+
+    public function getSubscribedPlanName(): ?string
+    {
+        return $this->getSubscribedPlanData()['name'] ?? null;
+    }
+
+    public function getSubscribedPlanPriceType(): ?string
+    {
+        $planData = $this->getSubscribedPlanData();
+
+        if ($stripeSubscription = $this->stripeSubscription) {
+            if ($planData['monthly_price'] === $stripeSubscription->stripe_price) {
+                    return 'monthly';
+            }
+
+            if ($planData['yearly_incentive'] === $stripeSubscription->stripe_price) {
+                return 'yearly';
+            }
+        }
+
+        return null;
+    }
+
+    private function getSubscribedPlanData(): ?array
+    {
+        if ($stripeSubscription = $this->stripeSubscription) {
             foreach (config('spark.billables.user.plans') as $idx => $plan) {
-                if (
-                    Subscription::where('user_id', $this->id)
-                        ->where('stripe_status', 'active')
-                        ->whereIn('stripe_price', [$plan['monthly_id'], $plan['yearly_id']])
-                        ->first()
-                ) {
-                    return $idx;
+                if (in_array($stripeSubscription->stripe_price, [$plan['monthly_id'], $plan['yearly_id']], true)) {
+                    $plan['idx'] = $idx;
+                    return $plan;
                 }
             }
         }
