00b20582a96f2b181f844384bb7045a5e7ade23b
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-17 00:35:20 +0100

    ko local

---
+++ lang/ko/subscription.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'incomplete_or_pending' => '결제 처리가 아직 완료되지 않았습니다. 시간이 조금 걸릴 수 있습니다. 이 과정이 너무 오래 걸린다면 은행에서 결제를 거부했을 수 있습니다. 이런 경우에는 <a href="/billing" class="font-semibold underline">다른 결제 방법</a>을 시도하거나 화면 오른쪽 하단의 지원팀에 문의해 주세요.',
+    'past_due' => '결제 실패. <a href="/billing" class="font-semibold underline">다른 결제 방법</a>을 시도하거나 나중에 결제를 다시 시도해 주세요.',
+    'trial_period_is_over' => '무료 체험 기간이 종료되었습니다. 서비스를 계속 사용하려면 <a href="/billing" class="font-semibold underline">구독 계획</a>을 선택하거나 <a href="/shop" class="font-semibold underline">적절한 제품</a>을 구매하세요.',
+
+];
