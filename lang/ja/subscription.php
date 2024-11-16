4f717bc055a53fc7cb0b6387aa20ebd482acc4f1
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-16 18:58:22 +0100

    ja local

---
+++ lang/ja/subscription.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'incomplete_or_pending' => 'お支払いが処理中です。時間がかかる場合があります。この処理が長時間続く場合、銀行が支払いを拒否した可能性があります。その場合は、<a href="/billing" class="font-semibold underline">別の支払い方法</a>を試すか、サポートにお問い合わせください（画面の右下隅）。',
+    'past_due' => '支払いに失敗しました。<a href="/billing" class="font-semibold underline">別の支払い方法を試す</a>か、後で再度支払いをお試しください。',
+    'trial_period_is_over' => '試用期間が終了しました。サービスを継続して使用するには、<a href="/billing" class="font-semibold underline">サブスクリプションプランを選択</a>するか、<a href="/shop" class="font-semibold underline">製品を購入</a>してください。',
+
+];
