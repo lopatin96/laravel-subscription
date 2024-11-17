aecf71b645fe114ece16acd9f664a45d85151e84
vladyslavlopatin <ilopatin3@gmail.com>
2024-11-17 01:14:05 +0100

    zh_CN local

---
+++ lang/zh_CN/subscription.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'incomplete_or_pending' => '您的支付仍在处理中。这可能需要一些时间。如果此过程花费太长时间，您的银行可能已拒绝您的支付。如果是这种情况，请尝试 <a href="/billing" class="font-semibold underline">更换支付方式</a> 或通过支持联系我我们（屏幕右下角）。',
+    'past_due' => '支付失败。<a href="/billing" class="font-semibold underline">尝试其他支付方式</a>，或稍后重新尝试支付。',
+    'trial_period_is_over' => '试用期已结束。要继续使用该服务，请 <a href="/billing" class="font-semibold underline">选择订阅计划</a> 或 <a href="/shop" class="font-semibold underline">购买</a> 适当的产品',
+
+];
